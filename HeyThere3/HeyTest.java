d1fb68b95eecf9b281c59613b551d8d299a3684c
ezgishnn <kilinc.ezgi@hotmail.com>
2022-07-05 20:17:49 -0400

    This is my first message

---
+++ HeyThere3/HeyTest.java
@@ -0,0 +1,7 @@
+package HeyThere3;
+
+public class HeyTest {
+    public static void main(String[] args) {
+        System.out.println("This is my page");
+    }
+}
---
+++ Kivi/ClassKivi.java
@@ -0,0 +1,8 @@
+package Kivi;
+
+public class ClassKivi {
+    public static void main(String[] args) {
+        System.out.println("Oglum Kivi");
+
+    }
+}

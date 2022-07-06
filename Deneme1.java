ce01a07ec74c5d0bef430e1781057df966640377
ezgishnn <kilinc.ezgi@hotmail.com>
2022-07-05 20:03:46 -0400

    kivi

--- Deneme1.java
+++ Deneme1.java
@@ -2,5 +2,9 @@ public class Deneme1 {
     public static void main(String[] args) {
         System.out.println("Heyy");
         System.out.println("This is my message");
+
+
+        System.out.println("have a good day");
+
     }
 }
